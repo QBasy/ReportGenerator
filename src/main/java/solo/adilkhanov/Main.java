7c84fabf1b41b05de8f4b889311e16f635abe30c
QoidynBasy <c0mplexie1@gmail.com>
2024-05-19 21:47:15 +0600

    SomeCleanCode

--- src/main/java/solo/adilkhanov/Main.java
+++ src/main/java/solo/adilkhanov/Main.java
@@ -2,8 +2,8 @@ package solo.adilkhanov;
 
 public class Main {
     public static void main(String[] args) {
-        String[] columns = {"title", "author", "publication_date", "genre"};
-        String tableName = "book";
+        String[] columns = {"username", "first_name", "active", "email"};
+        String tableName = "user_";
         DatabaseToXML.getXML(tableName, columns);
         ReportGenerator.generateReport(tableName+"_report.jrxml");
     }
